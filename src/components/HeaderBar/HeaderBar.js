3d8c86ce9783ad8c135d46a1785ed87c70f2039e
Renan Lopes <r.maruffa@gmail.com>
2023-08-11 13:10:28 -0300

    front end final

---
+++ src/components/HeaderBar/HeaderBar.js
@@ -0,0 +1,39 @@
+import { useLocation, useNavigate } from 'react-router-dom';
+import { goToLoginPage, goToPostsPage } from '../../routes/coordinator';
+import { TOKEN_NAME } from '../../constants/constants';
+import CloseIcon from '../../svgs/x-close-header.svg';
+import HeaderLogo from '../../svgs/logo-header.svg';
+import { HeaderBarContainer } from './styled'
+
+export default function HeaderBar() {
+    const location = useLocation()
+    const navigate = useNavigate()
+    const deslogar = () => {
+        window.localStorage.removeItem(TOKEN_NAME)
+        goToLoginPage(navigate)
+    }
+    const renderCorrectButtons = () => {
+        switch (location.pathname) {
+            case "/signup":
+                return <button onClick={() => goToLoginPage(navigate)}>Entrar</button>
+            case "/":
+                return <button onClick={deslogar}>Sair</button>
+            default:
+                return (
+                    <>
+                        <button onClick={() => goToPostsPage(navigate)}>
+                            <img className="close-icon" src={CloseIcon} alt="Voltar para posts" />
+                        </button>
+                        <button className="logout-btn" onClick={deslogar}>Sair</button>
+                    </>
+                )
+        }
+    }
+
+    return (
+        <HeaderBarContainer>
+            <img id="header-bar-logo" src={HeaderLogo} alt="LabEddit mini logo" />
+            {renderCorrectButtons()}
+        </HeaderBarContainer>
+    )
+};
